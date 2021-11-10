81d7c2353bcc2207f86588764d2ec5e7744cd681
MatheusOlivr <contato.matheusoliv@gmail.com>
2021-11-10 10:07:19 -0300

    atualizado

---
+++ [PHP]/T59/T59.php
@@ -0,0 +1,22 @@
+<form method="POST">
+	<label for = "inputPass">DIGITE UM ID:</label>
+	<br>
+	<input type = "text" name = "inputId" id = "inputId">
+	<br>
+	<button>CADASTRAR</button>
+</form>
+<?php
+	if ($_SERVER["REQUEST_METHOD"] === "POST")
+	{
+		$id = $_POST["inputId"];
+		$conexao = new PDO("mysql:dbname=bancodados;host=localhost","root","");
+		$stmt = $conexao->prepare("SELECT * FROM cadastro WHERE id = $id");
+		$stmt->execute();
+		$resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		echo "<br>";
+		print_r($resultado);
+	}
+?>
+<!--
+esse arquivo foi criado por um robo
+-->
